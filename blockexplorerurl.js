b905654d65071d5a70ec8e3eceb67d8bcfe3c73f
yoshinodev <100613025+yoshinodev@users.noreply.github.com>
2023-10-18 02:09:27 +0100

    testing fetching metamask pending status

---
+++ blockexplorerurl.js
@@ -0,0 +1,22 @@
+// The number of confirmations to wait for
+                // const confirmationsToWaitFor = 1;
+                // const transactionHash = transaction.transactionHash;
+                // Listen for transaction confirmation
+               /* const checkTransactionStatus = async () => {
+                try {
+                    const receipt = await web3.eth.getTransactionReceipt(transactionHash);
+                    if (receipt && receipt.status) {
+                        // The transaction has been confirmed and was successful
+                        document.getElementById('generationMessage').textContent = 'Your token was successfully created!';
+                        // You can also add a link to the transaction on a block explorer
+                        const blockExplorerLink = `https://testnet.bscscan.com/tx/${transactionHash}`;
+                        document.getElementById('blockExplorerLink').href = blockExplorerLink;
+                        document.getElementById('blockExplorerLink').classList.remove('d-none');
+                    }
+                } catch (error) {
+                    console.error("Error checking transaction status:", error);
+                }
+                };
+
+                // Call the function to check the transaction status
+                checkTransactionStatus();*/
